274bf6725114d49ca3da9c7b08f7836361c4379f
kmoon2437 <kmoon2437@gmail.com>
2021-11-29 12:36:45 +0900

    signature 검사하는 부분 수정

--- src/Reader.js
+++ src/Reader.js
@@ -21,8 +21,9 @@ module.exports = class Reader{
     verify_signature(){
         // 시그니처 안맞으면 퉤
         let sign = this.bs.read_bytes(Consts.KASUARI.length);
-        let tojson = JSON.stringify;
-        if(tojson(sign) != tojson(Consts.KASUARI)) throw new TypeError('bad signature bytes. I want kasuari');
+        if(!Consts.KASUARI.every((e,i,a) => sign[i] == a[i])){
+            throw new TypeError('bad signature bytes. I want kasuari');
+        }
     }
     
     read_text(){
